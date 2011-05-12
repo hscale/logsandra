54f53815653f807c17c33e9d3262d9d3a31abfcf
Tyler Hobbs <tyler@datastax.com>
2011-05-12 01:41:46 -0500

    Print info about keywords when loading sample data

--- scripts/fill_events.py
+++ scripts/fill_events.py
@@ -9,11 +9,12 @@ from datetime import datetime, timedelta
 
 from logsandra.model.client import CassandraClient
 
-
 client = CassandraClient('test', 'localhost', 9160, 3)
 
-today = datetime.now()
 keywords = ['foo', 'bar', 'baz']
+print "Loading sample data for the following keywords:", ', '.join(keywords)
+
+today = datetime.now()
 for i in range(1000):
     d = today + timedelta(randint(-7, -1), randint(-3600*24, 3600*24))
-    client.add_log(d, 'test entry', 'here', [keywords[i % 3]])
+    client.add_log(d, 'test entry', 'here', [keywords[i % len(keywords)]])
